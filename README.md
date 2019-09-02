2dcaf911ed2c3d002bdd74ef667fe3161df45b56
Dwi Fahni Denni <dfdenni0912@gmail.com>
2019-09-02 16:00:48 +0700

    First commit to master branch

---
+++ README.md
@@ -0,0 +1,50 @@
+# CONSUL FORCE LEAVE
+
+Schedule force-leave for unproperly removing "failed" node from consul cluster.
+
+## Concepts
+* When the nodes from consul members is on "left" state, it's actually still alive and it will eventually rejoin the cluster.
+* The `force-leave` is a method for remove "failed" nodes.
+* Consul will try to reconnect the "failed" nodes periodically by the time (default by 72 hours).
+* You can't rejoin the "failed" node until you running this `force-leave`.
+* References to [consul documentation](https://www.consul.io/docs/index.html)
+
+## Configurations
+* Configure / edit your all variable `CONSUL_SERVER` in all bash script (`consul-leave-daily.sh`, `consul-leave-dday.sh`, `consul-leave-weekly.sh`)
+  ```
+  CONSUL_SERVER="http://DNS-OR-IP-CONSUL-SERVER:8500"
+  ```
+  With your consul server address.
+* Setup cronjob
+  ```
+  crontab -e
+  ```
+* Add last line:
+  ```
+  ## Daily Force Leave every 30 minutes
+  */30 * * * * bash /root/consul-leave-dday.sh
+
+  ## Daily Force Leave at 1.30pm
+  30 1 * * * bash /root/consul-leave-daily.sh
+
+  ## Weekly Force Leave at 2.30pm Friday
+  30 2 * * 5 bash /root/consul-leave-weekly.sh
+  ```
+
+> **Notes:**
+> * You can copy `crontab.lst` to `/etc/cron.d/consul-backup` for default cronjob.
+>   ```
+>   sudo cp crontab.lst /etc/cron.d/consul-leave
+>   ```
+> -----
+
+## Running Manual
+```
+cd /root
+./consul-leave-daily.sh
+./consul-leave-dday.sh
+./consul-leave-weekly.sh
+```
+
+## License
+Apache version 2.0
---
+++ consul-leave-daily.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env sh
+# -----------------------------------------------------------------------------
+#  CONSUL FORCE LEAVE - CRON DAILY
+# -----------------------------------------------------------------------------
+#  Author     : Dwi Fahni Denni (@zeroc0d3)
+#  License    : Apache version 2.0
+# -----------------------------------------------------------------------------
+
+export CONSUL_SERVER="http://DNS-OR-IP-CONSUL-SERVER:8500"
+export PATH_LOG="/var/log/consul-force-leave"
+export PATH_DIR="daily"
+export PATH_LOG_CONSUL=$PATH_LOG"/"$PATH_DIR
+
+export SNAPSHOT_LOG=$(date +%Y-%m-%d)
+export LOG_FILE=$PATH_LOG_CONSUL"/"$SNAPSHOT_LOG.log
+
+run_check_members(){
+  echo "Get All failed node from consul members"
+  export MEMBERS=`consul members -http-addr=$CONSUL_SERVER | grep failed`
+}
+
+run_force_leaving(){
+  echo $MEMBERS
+  if [ ! -z "$MEMBERS" ]
+  then
+	  echo "Force Leave All Failed Node..."
+    echo "-------------------------------"
+	  consul members -http-addr=$CONSUL_SERVER | grep failed | awk '{ print $1 }' | xargs -L 1 consul force-leave -http-addr=$CONSUL_SERVER
+    echo "-------------------------------"
+    echo "Force Leave Done!"
+    run_create_log
+  fi
+}
+
+run_create_log(){
+  mkdir -p $PATH_LOG_CONSUL
+  echo "List of failed node \
+------------------------------------ \
+$MEMBERS \
+------------------------------------ \
+Execute at $SNAPSHOT_LOG" > $LOG_FILE
+}
+
+main() {
+  run_check_members
+  run_force_leaving
+}
+
+### START HERE ###
+main $@
